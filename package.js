da99926064fab75af29fd2c7336ca1a76759fde0
comerc <comerc@mail.ru>
2014-12-19 09:39:43 +0300

    placecomplete link

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'comerc:autoform-placecomplete',
   summary: 'Custom placecomplete input type for AutoForm',
-  version: '2.0.1',
+  version: '2.0.2',
   git: 'https://github.com/comerc/meteor-autoform-placecomplete.git'
 });
 
